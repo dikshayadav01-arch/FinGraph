d1ebe618d5601840aae059945239e9ffda52caa0
dikshayadav01-arch <dyadavofficial01@gmail.com>
2026-08-29 13:06:06 +0000

    Day 21: integrate Neo4j graph writer

---
+++ flink_jobs/src/main/java/com/fingraph/Neo4jWriter.java
@@ -0,0 +1,53 @@
+package com.fingraph;
+
+import com.fingraph.model.GraphTransaction;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
+
+public class Neo4jWriter implements AutoCloseable {
+
+    private final Driver driver;
+
+    public Neo4jWriter(String uri, String username, String password) {
+        this.driver = GraphDatabase.driver(
+                uri,
+                AuthTokens.basic(username, password)
+        );
+    }
+
+    public void writeTransaction(GraphTransaction transaction) {
+
+        try (Session session = driver.session()) {
+
+            session.run(
+                    """
+                    MERGE (sender:Account {id: $sender})
+                    MERGE (receiver:Account {id: $receiver})
+                    CREATE (sender)-[:SENT {
+                        transactionId: $transactionId,
+                        amount: $amount,
+                        transactionType: $transactionType,
+                        fraudStatus: $fraudStatus,
+                        riskLevel: $riskLevel
+                    }]->(receiver)
+                    """,
+                    org.neo4j.driver.Values.parameters(
+                            "sender", transaction.getSender(),
+                            "receiver", transaction.getReceiver(),
+                            "transactionId", transaction.getTransactionId(),
+                            "amount", transaction.getAmount(),
+                            "transactionType", transaction.getTransactionType(),
+                            "fraudStatus", transaction.getFraudStatus(),
+                            "riskLevel", transaction.getRiskLevel()
+                    )
+            );
+        }
+    }
+
+    @Override
+    public void close() {
+        driver.close();
+    }
+}
